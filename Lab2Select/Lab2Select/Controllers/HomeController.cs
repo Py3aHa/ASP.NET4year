671fff445d19f4a5e49e5d18be699a39ddd1dc02
Py3aHa <51872352+Py3aHa@users.noreply.github.com>
2020-10-03 21:27:55 +0600

    Add files via upload

---
+++ Lab2Select/Lab2Select/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2Select.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.val = true;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int select)
+        {
+            if(select == 1)
+            {
+                ViewBag.val = false;
+            }
+            else
+            {
+                ViewBag.val = true;
+            }
+            return View();
+        }
+        
+    }
+}
